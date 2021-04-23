7c7977fed3fd8faaef889cff1a376435e7accf6d
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-04-23 12:07:21 +0300

    Learn chapter4

---
+++ Chapter04/Ch04_01.py
@@ -0,0 +1,15 @@
+# %%
+def search4vowels(word: str) -> set():
+    """Search vowels in the input word."""
+    vowels = 'aeiou'
+    found = search4letters(word, vowels)
+    return found
+
+def search4letters(phrase:str, letters4search:str) -> set():
+    """Search the letters4search in the phrase."""
+    found = sorted(list(set(letters4search).intersection(set(phrase))))
+    return found
+
+# %%
+
+
