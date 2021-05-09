6689669ec4d78d70e9f48f012a83513c5471ec15
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-09 15:30:56 +0300

    Add logging function

---
+++ Chapter05/webapp/utils.py
@@ -0,0 +1,3 @@
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
--- Chapter05/webapp/vsearch4web.py
+++ Chapter05/webapp/vsearch4web.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from vsearch import search4letters
 from flask import render_template
+from utils import log_request
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ def do_seach() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html', the_title = title, the_results = results, the_phrase=phrase, the_letters = letters,)
 
 @app.route('/')
