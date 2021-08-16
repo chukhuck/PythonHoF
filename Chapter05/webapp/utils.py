5c8f1479887eb7a7ffa8d3f3f10160123511ba96
Alex Potapchuk <chuk.huck@ya.ru>
2021-08-16 09:08:11 +0300

    Delete unnessary field

--- Chapter05/webapp/utils.py
+++ Chapter05/webapp/utils.py
@@ -2,11 +2,6 @@ from DBcm import UseDatabase
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Log web-request and return the results."""
-    
-    dbconfig = {'host':'127.0.0.1', 
-                'user':'vsearch',
-                'password':'vsearchpasswd', 
-                'database': 'vsearchlogDB',}
 
     with UseDatabase(app.config['dbconfig']) as cursor:
         _SQL = """insert into log 
